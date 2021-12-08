d947d6597505026c78289efc0fe0d4196e449d43
Amin Moazzen <sma.moazzen@gmail.com>
2021-12-08 12:44:27 +0330

    Implement anti-aliasing.

---
+++ src/camera.rs
@@ -0,0 +1,39 @@
+use cliffy::Vec3;
+
+use crate::ray::Ray;
+
+pub struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+
+        let origin = Vec3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        Self {
+            origin,
+            vertical,
+            horizontal,
+            lower_left_corner: origin
+                - horizontal / 2.0
+                - vertical / 2.0
+                - Vec3::new(0.0, 0.0, focal_length),
+        }
+    }
+
+    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
+mod camera;
 mod hittable;
 mod hittable_list;
 mod ray;
 mod sphere;
 
+use camera::*;
 use cliffy::{Vec3, Vector};
 use hittable::Hittable;
 use hittable_list::HittableList;
 use image::{DynamicImage, GenericImage, Pixel};
+use rand::prelude::*;
 use ray::Ray;
 use sphere::Sphere;
+
+const RAND_MAX: f32 = 1.0;
+
+fn random() -> f32 {
+    let mut rng = StdRng::from_entropy();
+    rng.gen::<f32>() / (RAND_MAX + 1.0)
+}
+
+fn random_between(min: f32, max: f32) -> f32 {
+    min + (max - min) * random()
+}
+
+#[inline]
+fn clamp(x: f32, min: f32, max: f32) -> f32 {
+    let mut result = x;
+
+    if x < min {
+        result = min;
+    } else if x > max {
+        result = max;
+    }
+
+    result
+}
 
 fn ray_color(r: &Ray, world: &dyn Hittable) -> Vec3 {
     let (hit, rec) = world.hit(r, 0.0, f32::INFINITY);
@@ -23,11 +50,36 @@ fn ray_color(r: &Ray, world: &dyn Hittable) -> Vec3 {
     (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
 
+fn write_color(
+    image: &mut DynamicImage,
+    x: u32,
+    y: u32,
+    pixel_color: &Vec3,
+    samples_per_pixel: u32,
+) {
+    let mut r = pixel_color.x;
+    let mut g = pixel_color.y;
+    let mut b = pixel_color.z;
+
+    let scale = 1.0 / samples_per_pixel as f32;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    let ur = (256.0 * clamp(r, 0.0, 0.999)) as u8;
+    let ug = (256.0 * clamp(g, 0.0, 0.999)) as u8;
+    let ub = (256.0 * clamp(b, 0.0, 0.999)) as u8;
+
+    let pixel = Pixel::from_channels(ur, ug, ub, 255);
+    image.put_pixel(x, y, pixel);
+}
+
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f32 / aspect_ratio) as u32;
+    let samples_per_pixel = 100;
 
     let mut image = DynamicImage::new_rgb8(image_width, image_height);
 
@@ -37,29 +89,30 @@ fn main() {
     world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_lenght = 1.0;
-
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_lenght);
+    let cam = Camera::new();
 
     // Render
     let mut progress = 0;
     let total_pixels = image_width * image_height;
+    print!("{} / {}", progress, total_pixels);
+
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let u = i as f32 / (image_width - 1) as f32;
-            let v = j as f32 / (image_height - 1) as f32;
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
+            let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
+            for _s in 0..samples_per_pixel {
+                let u = (i as f32 + random()) / (image_width - 1) as f32;
+                let v = (j as f32 + random()) / (image_height - 1) as f32;
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+
+            write_color(
+                &mut image,
+                i,
+                image_height - 1 - j,
+                &pixel_color,
+                samples_per_pixel,
             );
-            let pixel_color = ray_color(&r, &world);
-            write_color(&mut image, i, image_height - 1 - j, &pixel_color);
 
             progress += 1;
         }
@@ -70,13 +123,4 @@ fn main() {
     let image_path = "./output.png";
 
     image.save(&image_path).unwrap();
-}
-
-fn write_color(image: &mut DynamicImage, x: u32, y: u32, pixel_color: &Vec3) {
-    let ur = (255.999 * pixel_color.x) as u8;
-    let ug = (255.999 * pixel_color.y) as u8;
-    let ub = (255.999 * pixel_color.z) as u8;
-
-    let pixel = Pixel::from_channels(ur, ug, ub, 255);
-    image.put_pixel(x, y, pixel);
 }
